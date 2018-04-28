b466d0e2adfb708bd9fc77f365f795b94572b3fe
bj- <bmw518@bk.ru>
2018-04-28 23:01:32 +0300

    examples

---
+++ Examples/Basic/Blink.lua
@@ -0,0 +1,12 @@
+ighton=0
+pin=4
+gpio.mode(pin,gpio.OUTPUT)
+tmr.alarm(1,500,1,function()
+	if lighton==0 then
+		lighton=1
+		gpio.write(pin,gpio.HIGH)
+	else
+		lighton=0
+		gpio.write(pin,gpio.LOW)
+	end
+end)
